f87d47a525cc37b6154a999e758115760c1ce891
yarden zehavi <yarden.goldy@gmail.com>
2022-08-10 01:17:54 +0300

    add server test

---
+++ server/components/ticket/ticketApi.test.ts
@@ -0,0 +1,17 @@
+
+import * as fs from "fs";
+describe("test add function", () => {
+  it("should return 15 for add(10,5)", () => {
+    expect(15).toBe(15);
+  });
+
+  it("should return 5 for add(2,3)", () => {
+    expect(5).toBe(5);
+  });
+});
+
+describe("test mul function", () => {
+  it("should return 15 for mul(3,5)", () => {
+    expect(15).toBe(15);
+  });
+});
